9057e08db4dcbb1fae2c34ff0e026824feb10821
Alexander Cherepanov <xandr.cherepanov@gmail.com>
2026-06-14 00:22:35 +0500

    task 8: add sql scripts

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE cart_status AS ENUM ('OPEN', 'ORDERED');
+
+CREATE TABLE IF NOT EXISTS carts (
+  id         uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id    uuid NOT NULL,
+  created_at date NOT NULL DEFAULT CURRENT_DATE,
+  updated_at date NOT NULL DEFAULT CURRENT_DATE,
+  status     cart_status NOT NULL DEFAULT 'OPEN'
+);
+
+CREATE TABLE IF NOT EXISTS cart_items (
+  cart_id    uuid NOT NULL REFERENCES carts(id),
+  product_id uuid NOT NULL,
+  count      integer NOT NULL
+);
